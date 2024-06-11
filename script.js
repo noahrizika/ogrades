d6bcb7d9f16bca6fdb718af688ab833f51b892f6
noahrizika <noahrizika@gmail.com>
2024-06-10 22:07:55 -0700

    update style

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
-// function toggleMenu() {
-//     const menu = document.querySelector(".menu-links");
-//     const icon = document.querySelector(".hamburger-icon");
-//     menu.classList.toggle("open");
-//     icon.classList.toggle("open");
-//   }
-
 function animateCount(target, duration) {
-    console.log('here');
     if (!isNaN(target))
     {
         const start = 0;
